bcc8443b24ca5fc46b22c4e696bc6a6904db9ff2
nhan-ho-saltovn <nhan.ho@salto.vn>
2022-10-20 10:35:02 +0700

    config tailwind

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/auth.context";
 import { GameProvider } from "./context/game.context";
