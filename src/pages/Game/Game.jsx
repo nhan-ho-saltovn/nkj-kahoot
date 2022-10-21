3de28e34566dd7080b37ddeb51c90c5ce70276e0
nhan-ho-saltovn <nhan.ho@salto.vn>
2022-10-21 15:06:37 +0700

    revert status

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -5,7 +5,7 @@ import QuizBoard from "../../components/QuizBoard/QuizBoard";
 import WaitingDashboard from "../../components/WaitingDashboard/WaitingDashboard";
 
 const Game = () => {
-  const [isStart, setIsStart] = useState("STARTING");
+  const [isStart, setIsStart] = useState("WAITING");
 
   const unsub = onSnapshot(
     doc(db, "questionList", "Test1", "games", "game1"),
